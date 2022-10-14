0441c59b3e9d7629c62f7b09af55b95d7d0b539f
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-10-14 10:17:46 +0100

    Check something

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -20,24 +20,26 @@ def display_hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>')
+@app.route("/c/<text>", strict_slashes=False)
 def display_c(text):
-    """Print route param to display"""
-    return f"C {text.replace('_', ' ')}"
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def display_python(text="is cool"):
     """Print route param to display"""
-    return f"Python {text.replace('_', ' ')}"
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 @app.route('/number/<int:n>')
 def display_n(n):
     """Print n if n is int"""
-    return f"{n} is a number"
+    return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
