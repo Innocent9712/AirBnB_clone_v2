1f0fbd5609cce01d64c900331244cee3ff5759d4
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-10-14 10:16:19 +0100

    Check something

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -20,17 +20,19 @@ def display_hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>')
+@app.route("/c/<text>", strict_slashes=False)
 def display_c(text):
-    """Print route param to display"""
-    return f"C {text.replace('_', ' ')}"
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def display_python(text="is cool"):
     """Print route param to display"""
-    return f"Python {text.replace('_', ' ')}"
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 if __name__ == "__main__":
