fc5ff144da7f2176488874b91e179152d3958671
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-10-14 10:19:04 +0100

    Check something

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,8 +2,7 @@
 """
 A simple flask server running on 0.0.0.0:5000
 """
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -21,23 +20,25 @@ def display_hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>')
+@app.route("/c/<text>", strict_slashes=False)
 def display_c(text):
-    """Print route param to display"""
-    return f"C {text.replace('_', ' ')}"
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def display_python(text="is cool"):
     """Print route param to display"""
-    return f"Python {text.replace('_', ' ')}"
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 @app.route('/number/<int:n>')
 def display_n(n):
     """Print n if n is int"""
-    return f"{n} is a number"
+    return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>')
@@ -47,4 +48,4 @@ def display_n_template(n):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
