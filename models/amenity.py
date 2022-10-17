5271f4aa95b11eaf62e3dfb01cf0f7d11ec06508
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-10-17 07:52:23 +0100

    Fix bug

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
@@ -13,7 +11,7 @@ class Amenity(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-    # place_amenities = relationship("Place", secondary=place_amenity)
+        place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
 
     else:
         name = ""
