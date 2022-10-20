4b5eef9e4482f5fe6f638acae94e6a3f87d0c763
root <stevanion2013@gmail.com>
2022-10-20 16:45:15 -0700

    web framework

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
-"""
-<<<<<<< HEAD
-starts a Flask web application
-"""
-
-from flask import Flask
-=======
-A simple flask server running on 0.0.0.0:5000
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
 """
 from flask import Flask
 
->>>>>>> d364182adf6b6aebc07fd37d00692d0bd7608f44
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-<<<<<<< HEAD
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-=======
-def display():
-    """Prints 'Hello HBNB!' to display"""
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
->>>>>>> d364182adf6b6aebc07fd37d00692d0bd7608f44
+    app.run(host="0.0.0.0")
